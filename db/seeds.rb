f5cfb0c645def8f1dd3962e3b82ec480ab078200
Adam Doyle <dominofoe@gmail.com>
2014-10-03 04:34:06 +0000

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,18 @@ require 'faker'
     user.save!
 end
 users = User.all
+
+# Create Topics
+15.times do
+    Topic.create!(
+        name:               Faker::Lorem.sentence,
+        description:  Faker::Lorem.paragraph
+        )
+end
+topics = Topic.all
+
+
+
 # Note: by calling `User.new` instead of `create`,
 # we create an instance of User which isn't immediately saved to the database.
 
@@ -24,6 +36,7 @@ users = User.all
  50.times do
    Post.create!(
      user:  users.sample,
+     topic: topics.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -39,10 +52,34 @@ users = User.all
    )
  end
 
- User.first.update_attributes!(
-    email: 'dominofoe@yahoo.com',
-    password: 'password',
+ # Create admin user
+ admin = User.new(
+    name:               'Admin User',
+    email:               'admin@example.com',
+    password:      'helloworld',
+    role:                   'admin'
     )
+ admin.skip_confirmation!
+ admin.save!
+
+ # Create moderator
+ moderator = User.new(
+     name:          'Moderator User',
+     email:          'moderator@example.com', 
+     password: 'helloworld',
+     role:              'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save!
  
  puts "Seed finished"
  puts "#{User.count} users created"
